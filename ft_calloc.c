97f0309ecd560fa2d765ca15e26fa8acc8062f95
Johann Herzog <jherzog@3-F-5.42heilbronn.de>
2021-10-08 12:40:30 +0200

    wright function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 16:13:34 by jherzog           #+#    #+#             */
-/*   Updated: 2021/09/08 16:16:16 by jherzog          ###   ########.fr       */
+/*   Updated: 2021/10/08 12:39:36 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*output;
+	int		len;
 
-	output = (void *)malloc(count * size);
+	len = count * size;
+	output = malloc(len);
 	if (!output)
 	{
 		return (NULL);
 	}
-	ft_bzero(output, count);
+	ft_bzero(output, len);
 	return (output);
 }
