7e7169c8f98d047607bf703884bbc57e1af7af91
Tachiguro <Tachiguro@gmail.com>
2022-04-06 12:07:19 +0200

    Update ft_isalnum.c
    docu

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -10,6 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/*  Function: ft_isalnum                                                      */
+/*  --------------------                                                      */
+/*  The isalnum() function Checks if the given character is an alphanumeric   */
+/*  character                                                                 */
+/*                                                                            */
+/*  c: character to be checked                                                */
+/*  return: Non-zero value if the c is a alphanumeric, zero otherwi           */
+/* ************************************************************************** */
+
 int	ft_isalnum(int c)
 {
 	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
