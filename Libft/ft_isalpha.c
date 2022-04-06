5bdbda0a8a73885a43c10caf7a283d1c904489e0
Tachiguro <Tachiguro@gmail.com>
2022-04-06 11:44:35 +0200

    Added docu

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -10,6 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** */
+/*	Function: ft_isalpha													  */
+/*	--------------------													  */
+/*	The isalpha() function checks whether a character is an alphabet or not.  */
+/*																			  */
+/*  c: character to be checked												  */
+/*	return: Non-zero value if the character is an alphabetic character,		  */
+/*		zero otherwise.														  */
+/* ************************************************************************** */
+
 int	ft_isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
