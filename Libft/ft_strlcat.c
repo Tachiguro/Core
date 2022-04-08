fd76b037b0d007e00e1c14326a491b8ae26d1ee4
Tachiguro <Tachiguro@gmail.com>
2022-04-08 15:31:23 +0200

    Update ft_strlcat.c
    docu

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -12,6 +12,18 @@
 
 #include "libft.h"
 
+/* ************************************************************************** */
+/*  Function: ft_strlcat                                                      */
+/*  --------------------                                                      */
+/*  The ft_strlcat() function cancatenate src to string dst of size len.      */
+/*                                                                            */
+/*  dst: A pointer to the destination string.                                 */
+/*  src: A pointer to the source string.                                      */
+/*  len: The size of the destination buffer.                                  */
+/*  return: The total length of the string initial length of dst plus         */
+/*    the length of src. While this may seem somewhat confusing, it was done  */
+/*    to make truncation detection simple.                                    */
+/* ************************************************************************** */
 size_t	ft_strlcat(char *dst, const char *src, size_t len)
 {
 	size_t	i;
