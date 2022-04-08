0e61e44097ce4a438290292bf694b2ada442812e
Tachiguro <Tachiguro@gmail.com>
2022-04-08 15:08:46 +0200

    Update ft_bzero.c
    docu update

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -15,7 +15,7 @@
 /* ************************************************************************** */
 /*  Function: ft_bzero                                                        */
 /*  --------------------                                                      */
-/*  The bzero() function erases the data in the n bytes of the memory         */
+/*  The ft_bzero() function erases the data in the n bytes of the memory      */
 /*  starting at the location pointed to by s, by writing zeros (bytes         */
 /*  containing '\0') to that area.                                            */
 /*                                                                            */
