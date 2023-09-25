1f56d1ddfb9f3bec4ea517e07440705d4f17a331
Johann Herzog <jherzog@Tachis-Mac.local>
2023-09-26 00:06:31 +0200

    better ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:28:41 by jherzog           #+#    #+#             */
-/*   Updated: 2023/07/13 18:30:03 by jherzog          ###   ########.fr       */
+/*   Updated: 2023/09/25 23:54:42 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@
 char	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t		i;
-	char		*chr;
-	char		*str;
+	const char	*str;
 
-	str = (char *)s;
+	str = (const char *)s;
 	i = 0;
 	while (i < n)
 	{
 		if (str[i] == (char)c)
-		{
-			chr = (char *)&s[i];
-			return (chr);
-		}
+			return ((char *)&str[i]);
 		i++;
 	}
 	return (NULL);
