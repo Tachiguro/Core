24e97fc9732a92c14fdcb2c3ca8835c6eeba4009
Johann Herzog <jherzog@Tachis-Mac.local>
2023-11-10 00:41:08 +0100

    fixed ex13 added ex14

--- piscine_reloaded/ex14/ft_sqrt.c
+++ piscine_reloaded/ex14/ft_sqrt.c
@@ -1,37 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 17:20:51 by jherzog           #+#    #+#             */
-/*   Updated: 2023/11/10 00:40:04 by jherzog          ###   ########.fr       */
+/*   Created: 2023/11/09 17:52:51 by jherzog           #+#    #+#             */
+/*   Updated: 2023/11/10 00:39:43 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_recursive_factorial(int nb)
+int	ft_sqrt(int nb)
 {
-	if (nb == 0)
-		return (1);
-	else if (nb < 0 || nb > 12)
+	int	l;
+	int	r;
+	int	mid;
+
+	l = 0;
+	r = nb;
+	if (nb < 0)
 		return (0);
-	else
-		return (nb * ft_recursive_factorial(nb -1));
+	if (nb == 0 || nb == 1)
+		return (nb);
+	while (l <= r)
+	{
+		mid = l + (r - l) / 2;
+		if (nb == mid * mid)
+			return (mid);
+		else if (nb > mid * mid)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return (0);
 }
 
+//     if ((long long)(l - 1) * (long long)(l - 1) == nb) {
+//         return l - 1;
+//     }
+
 // #include <stdio.h>
+// #include <limits.h>
 
 // int	main(void)
 // {
 // 	int	i;
-
-// 	i = 20;
-// 	while (i >= 0)
-// 	{
-// 	printf("factorial: %d i: %d\n", ft_recursive_factorial(i), i);
-// 	i--;
-// 	}
-
+// 	i = INT_MAX;
+// 	printf("sqrt: %d i: %d\n", ft_sqrt(i), i);
 // 	return (0);
 // }
