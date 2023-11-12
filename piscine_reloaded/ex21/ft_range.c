e5dd8726cdc12ad901f5c1c27d89ffe33eaa1ac5
Johann Herzog <jherzog@Tachis-Mac.local>
2023-11-12 18:02:12 +0100

    added ex21

---
+++ piscine_reloaded/ex21/ft_range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 17:59:34 by jherzog           #+#    #+#             */
+/*   Updated: 2023/11/12 18:01:46 by jherzog          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*arr;
+	int	i;
+
+	if (min >= max)
+		return (0);
+	arr = (int *)malloc((max - min) * sizeof(int));
+	if (!arr)
+		return (0);
+	i = 0;
+	while (min < max)
+	{
+		arr[i] = min;
+		min++;
+		i++;
+	}
+	return (arr);
+}
