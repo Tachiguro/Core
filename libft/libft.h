f28c16c7b2d27c7f408091276ad3041569f32d89
Johann Herzog <jherzog@c2s14d75.42wolfsburg.de>
2023-11-16 14:01:25 +0100

    added ft_lstdelone.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:29:56 by jherzog           #+#    #+#             */
-/*   Updated: 2023/11/13 20:57:55 by jherzog          ###   ########.fr       */
+/*   Updated: 2023/11/16 12:13:48 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
--- test_libft.c
+++ test_libft.c
@@ -6,37 +6,53 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:58:34 by jherzog           #+#    #+#             */
-/*   Updated: 2023/11/14 18:48:42 by jherzog          ###   ########.fr       */
+/*   Updated: 2023/11/16 13:59:20 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "./libft/libft.h"
 
+void	del(void *content);
+
 int	main(void)
 {
 	t_list	*list;
 	t_list	*temp;
+	t_list	*third;
 
-	list = ft_lstnew("Test1");
-	printf("Content: %s, Next: %p\n", (char *)list->content, list->next);
+	list = ft_lstnew("TEST1");
+	ft_lstadd_back(&list, ft_lstnew("TEST2"));
+	ft_lstadd_front(&list, ft_lstnew("TEST0"));
+	ft_lstadd_back(&list, ft_lstnew("TEST3"));
 
-	ft_lstadd_front(&list, ft_lstnew("Test0"));
-	printf("ft_lstadd_front: New front: %s\n", (char *)list->content);
+	printf("Size:%d\n", ft_lstsize(list));
 
-	ft_lstadd_back(&list, ft_lstnew("Test2"));
-	temp = ft_lstlast(list);
-	printf("ft_lstadd_back: New back: %s\n", (char *)temp->content);
-
-	printf("ft_lstsize: Size of list: %d\n", ft_lstsize(temp));
-
+	printf("List contents after creating: \n");
 	temp = list;
-	printf("List contents: ");
-	while (temp != NULL)
-	{
+	while (temp != NULL) {
+		printf("%s ", (char *)temp->content);
+		temp = temp->next;
+	}
+	printf("\n");
+
+	// Example of deleting a node, e.g., the second node
+	temp = list->next; // Get the second node
+	list->next = temp->next; // Adjust the 'next' pointer of the first node
+
+	ft_lstdelone(temp, del); // Delete the second node
+
+	printf("List contents at the end: \n");
+	temp = list; // Reset the temporary pointer
+	while (temp != NULL) {
 		printf("%s ", (char *)temp->content);
 		temp = temp->next;
 	}
 	printf("\n");
 	return (0);
+}
+
+void	del(void *content)
+{
+	free(content);
 }
