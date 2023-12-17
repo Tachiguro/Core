be839f4e03542fe0123e7d3ef9fbbc1bb02ca0a1
Johann Herzog <jherzog@Tachis-Mac.local>
2023-12-17 19:17:57 +0100

    fixed to secure ft_print_int

--- ft_printf/src/ft_print_int.c
+++ ft_printf/src/ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 23:44:14 by jherzog           #+#    #+#             */
-/*   Updated: 2023/12/16 21:24:50 by jherzog          ###   ########.fr       */
+/*   Updated: 2023/12/17 19:17:30 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int	ft_print_int(int n)
 {
-	int				counter;
-	unsigned int	u_int;
-	int				temp;
+	int	counter;
+	int	temp;
 
 	counter = 0;
 	temp = 0;
@@ -24,18 +23,20 @@ int	ft_print_int(int n)
 		return (ft_print_str("-2147483648"));
 	if (n < 0)
 	{
-		u_int = -n;
+		n = -n;
 		counter = ft_print_char('-');
 		if (counter == -1)
 			return (-1);
 	}
-	else
-		u_int = n;
-	if (u_int > 9)
-		counter += ft_print_int(u_int / 10);
-	temp = ft_print_char((u_int % 10) + '0');
+	if (n > 9)
+	{
+		temp = ft_print_int(n / 10);
+		if (temp == -1)
+			return (-1);
+		counter += temp;
+	}
+	temp = ft_print_char((n % 10) + '0');
 	if (temp == -1)
 		return (-1);
-	counter += temp;
-	return (counter);
+	return (counter + temp);
 }
