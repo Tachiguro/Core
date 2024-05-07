f6eb0e2ea2850c28820e5fd6fbe61c6148262932
Johann Herzog <jherzog@Tachis-Mac.fritz.box>
2024-05-07 22:32:01 +0200

    added create_stack function

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,52 +6,107 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 18:47:12 by jherzog           #+#    #+#             */
-/*   Updated: 2024/05/07 17:55:06 by jherzog          ###   ########.fr       */
+/*   Updated: 2024/05/07 22:31:01 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include <stdio.h>
 
-// int	*create_stack(char *numbers, char *argv[])
+// int	check_correct_input(char *argv, char ***strs)
 // {
-// 	int len;
+// 	int	i;
 
-// 	len = ft_strlen(numbers);
+// 	i = 0;
+// 	*strs = ft_split(argv, ' ');
+// 	while (strs[i] && strs[i + 1])
+// 	{
+// 		if (ft_strncmp((*strs)[i], (*strs)[i + 1], ft_strlen((*strs)[i])) == 0)
+// 			exit(-1);
+// 		i++;
+// 	}
+
+// 	return (1);
 // }
 
-int	check_correct_input(char *argv, char **strs)
+int	create_stack(char *param, int **stack_a)
 {
-	int	i;
+	int		len;
+	int		i;
+	int		*stack;
+	char	**strs;
 
+	len = 0;
 	i = 0;
-	strs = ft_split(argv, ' ');
-	while (strs[i] && strs[i + 1])
+	strs = ft_split(param, ' ');
+	while (strs[len])
+		len++;
+	stack = (int *)malloc(sizeof(int) * len);
+	if (!stack)
+		exit(-1);
+	while (i < len)
 	{
-		if (ft_strncmp(strs[i], strs[i + 1], ft_strlen(strs[i])) == 0)
-			exit(-1);
+		stack[i] = ft_atol(strs[i]);
 		i++;
 	}
+	*stack_a = stack;
+	free(strs);
+	return (len);
+}
 
-	return (1);
+char	*join_args(char **argv)
+{
+	char	*all_parametres;
+	int		len;
+	int		y;
+	int		x;
+	int		i;
+
+	len = 0;
+	y = 1;
+	x = 0;
+	i = 0;
+	while (argv[y])
+		len += ft_strlen(argv[y++]) + 1;
+	all_parametres = (char *)malloc(sizeof(char) * len);
+	if (!all_parametres)
+		exit(-1);
+	y = 0;
+	while (argv[++y])
+	{
+		while (argv[y][x])
+			all_parametres[i++] = argv[y][x++];
+		all_parametres[i++] = ' ';
+		x = 0;
+	}
+	all_parametres[++i] = '\0';
+	return (all_parametres);
 }
 
 int	main(int argc, char **argv)
 {
-	char	**strs;
+	char	*all_parameters;
+	int		*stack_a;
+	int		stack_len;
 	int		i;
 
-	i = 0;
-	strs = NULL;
+	i = -1;
+	all_parameters = NULL;
+	stack_a = NULL;
+	stack_len = 0;
 	if (argc == 1)
 		exit(1);
+	else if (argc == 2)
+		stack_len = create_stack(argv[1], &stack_a);
 	else
 	{
-		check_correct_input(*argv++, strs);
+		all_parameters = join_args(argv);
+		stack_len = create_stack(all_parameters, &stack_a);
+		free(all_parameters);
 	}
-	while (strs[i])
+	while (++i < stack_len)
 	{
-		printf("argv:%s\n", strs[i]);
+		printf("stack_a:%i\n", stack_a[i]);
 	}
-	return (0);
+	free(stack_a);
 }
