fd35d2752e0c6e65281416441ad45e61996eac9f
Johann Herzog <jherzog@Tachis-Mac.fritz.box>
2024-05-07 18:02:10 +0200

    Added check function

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,11 +6,12 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 18:47:12 by jherzog           #+#    #+#             */
-/*   Updated: 2024/05/06 20:17:26 by jherzog          ###   ########.fr       */
+/*   Updated: 2024/05/07 17:55:06 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+#include <stdio.h>
 
 // int	*create_stack(char *numbers, char *argv[])
 // {
@@ -19,35 +20,38 @@
 // 	len = ft_strlen(numbers);
 // }
 
-int	check_correct_input(int argc, char *argv[])
+int	check_correct_input(char *argv, char **strs)
 {
 	int	i;
-	int	len;
 
 	i = 0;
-	len = 0;
-	while (++len < argc)
+	strs = ft_split(argv, ' ');
+	while (strs[i] && strs[i + 1])
 	{
-		i += ft_strlen(argv[len]);
+		if (ft_strncmp(strs[i], strs[i + 1], ft_strlen(strs[i])) == 0)
+			exit(-1);
+		i++;
 	}
-	return (i);
+
+	return (1);
 }
 
-#include <stdio.h>
-
-int	main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
-	// int	*stack_a;
-	// int	*stack_b;
-	int i;
+	char	**strs;
+	int		i;
 
 	i = 0;
+	strs = NULL;
 	if (argc == 1)
 		exit(1);
 	else
 	{
-		i = check_correct_input(argc, argv);
-		printf("i = %i\n", i);
+		check_correct_input(*argv++, strs);
+	}
+	while (strs[i])
+	{
+		printf("argv:%s\n", strs[i]);
 	}
 	return (0);
 }
