1f34c127a42054ccba2a8e35b6982be601097a8a
Johann Herzog <jherzog@Tachis-Mac.fritz.box>
2024-09-09 20:59:32 +0200

    Not working, but i get the idea

--- Minitalk/client_dir/client.c
+++ Minitalk/client_dir/client.c
@@ -6,31 +6,58 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 18:47:19 by jherzog           #+#    #+#             */
-/*   Updated: 2024/09/09 20:26:14 by jherzog          ###   ########.fr       */
+/*   Updated: 2024/09/09 20:51:24 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/client.h"
 
+static void	handle_error(char *msg)
+{
+	ft_printf("%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+static void	send_signal(pid_t server_pid, char *message)
+{
+	int		i;
+	char	c;
+	int		bit;
+
+	i = -1;
+	while (message[++i] != '\0')
+	{
+		c = message[i];
+		bit = 0;
+		while (bit < 8)
+		{
+			if ((c >> bit) & 1)
+			{
+				if (kill(server_pid, SIGUSR1) == -1)
+					handle_error("Failed to send SIGUSR1!");
+			}
+			else
+			{
+				if (kill(server_pid, SIGUSR2) == -1)
+					handle_error("Failed to send SIGUSR2!");
+			}
+			bit++;
+			usleep(100);
+		}
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-	{
-		ft_printf("Error: Not enouth arguments!\n");
-		ft_printf("Usage:\n\t>%s <server pid> \"message\"\n", argv[0]);
-		return(1);
-	}
-	else if (argc > 2)
-	{
-		ft_printf("Error: Too much arguments!\n");
-		ft_printf("Usage:\n\t>%s <server pid> \"message\"\n", argv[0]);
-		return(1);
-	}
-	if (argv[1][0] == '\0')
-	{
-		ft_printf("Error: Empty string\n");
-		ft_printf("Usage:\n\t>%s <server pid> \"message\"\n", argv[0]);
-		return(1);
-	}
+	pid_t	server_pid;
+
+	if (argc != 3)
+		handle_error("Usage: ./client <server_pid> \"message\"");
+	server_pid = (pid_t)ft_atoi(argv[1]);
+	if (server_pid <= 0)
+		handle_error("Invalid PID!");
+	if (argv[2][0] == '\0')
+		handle_error("Error: Message cannot be empty!");
+	send_signal(server_pid, argv[2]);
 	return (0);
 }
--- Minitalk/server_dir/server.c
+++ Minitalk/server_dir/server.c
@@ -6,21 +6,51 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 18:47:53 by jherzog           #+#    #+#             */
-/*   Updated: 2024/09/09 20:12:29 by jherzog          ###   ########.fr       */
+/*   Updated: 2024/09/09 20:50:58 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/server.h"
 
-int	main(int argc, char **argv)
+void	handle_signal(int sig, siginfo_t *info, void *context)
 {
-	if (argc > 1)
+	static int				bit_index = 0;
+	static unsigned char	character = 0;
+
+	(void)context;
+	if (sig == SIGUSR1)
+		character |= (1 << bit_index);
+	bit_index++;
+	if (bit_index == 8)
 	{
-		ft_printf("To much arguments!\n");
-		exit(1);
+		ft_printf("%c", character);
+		if (character == '\0')
+			ft_printf("\n");
+		bit_index = 0;
+		character = 0;
 	}
-	if (!argv[0])
-		exit(1);
+	kill(info->si_pid, SIGUSR1);
+}
+
+void	handle_error(char *msg)
+{
+	ft_printf("%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
 	ft_printf("Server PID: %d\n", getpid());
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = &handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		handle_error("Error: Unable to catch SIGUSR1");
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		handle_error("Error: Unable to catch SIGUSR2");
+	while (1)
+		pause();
 	return (0);
 }
