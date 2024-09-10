c301fc3c61429bd0bfc006f72497577dfce36242
Johann Herzog <jherzog@Tachis-Mac.fritz.box>
2024-09-10 23:00:13 +0200

    Minitalk is finished :)

--- Minitalk/includes/server.h
+++ Minitalk/includes/server.h
@@ -6,14 +6,13 @@
 /*   By: jherzog <jherzog@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 18:49:44 by jherzog           #+#    #+#             */
-/*   Updated: 2024/09/10 18:08:52 by jherzog          ###   ########.fr       */
+/*   Updated: 2024/09/10 22:30:42 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
-# include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
 # include <signal.h>
 
