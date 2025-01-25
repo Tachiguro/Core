02c056c79013b7a5a518af9de1071a95da4ce2f4
Tachiguro <tachiguro@gmail.com>
2025-01-25 23:33:04 +0100

    removed old stuff, rebase, created new aproach

--- sh/sh.h
+++ sh/sh.h
@@ -11,4 +11,29 @@
 bool	is_correct_syntax(const char *l);
 // Error handling
 void	print_error(const char *text);
+
+// Tokenization
+typedef enum	e_type
+{
+	T_SINGLE_QUOTE =	'\'',
+	T_DOUBLE_QUOTE =	'\"',
+	T_LESS_THAN =		'<',
+	T_GREATER_THAN =	'>',
+	T_PIPE =			'|',
+	T_DOLLAR =			'$',
+	T_QUESTION_MARK =	'?',
+	T_TILDE =			'~',
+	T_DWORD =			' '
+}				t_symbol;
+
+typedef struct	s_token
+{
+	t_symbol		type;
+	char			*value;
+	struct s_token	*next
+} t_token;
+
+t_token	*get_token(char *line);
+// Lexer
+
 #endif
--- sh/tokenization.c
+++ sh/tokenization.c
@@ -6,7 +6,61 @@
 /*   By: jherzog <jherzog@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 17:32:28 by jherzog           #+#    #+#             */
-/*   Updated: 2025/01/22 17:32:36 by jherzog          ###   ########.fr       */
+/*   Updated: 2025/01/25 23:02:03 by jherzog          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "sh.h"
+
+static bool	is_special_char(char c)
+{
+	if (c == '\'' || c == '\"' || c == '<' || c == '>' || c == '|' || c == '$')
+		return (true);
+	return (false);
+}
+
+static t_symbol	char_to_symbor(char c)
+{
+	if (c == '|')
+		return (T_PIPE);
+	if (c == '<')
+		return (T_LESS_THAN);
+	if (c == '>')
+		return (T_GREATER_THAN);
+	if (c == '$')
+		return (T_DOLLAR);
+	if (c == '\'')
+		return (T_SINGLE_QUOTE);
+	if (c == '\"')
+		return (T_DOUBLE_QUOTE);
+	if (c == '?')
+		return (T_QUESTION_MARK);
+	if (c == '~')
+		return (T_TILDE);
+	return (T_WORD);
+}
+
+static t_token	add_token(char *line)
+{
+
+}
+
+t_token	*get_token(char *line)
+{
+	t_token	*head;
+	int		i;
+	bool	in_quotes;
+
+	head = NULL;
+	i = 0;
+	in_quotes = false;
+	while (line[i] != '\0')
+	{
+		if (is_special_char(line[i]))
+		{
+
+		}
+		i++;
+	}
+	return (head);
+}
